a4b554086d706168868d68191dd97c69ac4c36eb
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-06-06 23:53:40 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -48,20 +48,20 @@ var summaries = new[]
 };
 
 // Endpoint: GET /weatherforecast
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast = Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
 
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
 
 // Dummy endpoint: /api/todo
 var todos = new List<string> { "Belajar .NET", "Ngoding Flutter" };
